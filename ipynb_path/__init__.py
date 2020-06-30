731334b4fc7336da3c96e443844030ff340de447
Kazuma Takahara <4269kzm@gmail.com>
2020-06-30 09:28:20 +0900

    Fix arguments of ipynb_path.get()

--- ipynb_path/__init__.py
+++ ipynb_path/__init__.py
@@ -1,7 +1,7 @@
-def get(name='__main__'):
+def get(name='__main__', password=None):
     import sys
     try:
         return sys.modules[name].__file__
     except AttributeError:
         from . import core
-        return core.current_notebook_path()
+        return core.current_notebook_path(password=password)
